436a2fdca968f8d81a33b061ae9beb85d6c28b5f
YoshinoriN <nayo-msoft@outlook.jp>
2018-11-19 23:33:24 +0900

    Implement route trait

---
+++ selfouettellia-server/src/main/scala/net/yoshinorin/selfouettellia/Route.scala
@@ -0,0 +1,13 @@
+package net.yoshinorin.selfouettellia
+
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+
+trait Route {
+
+  val route = get {
+    pathEndOrSingleSlash {
+      complete(HttpEntity(ContentTypes.`application/json`, "{\"message\": \"API is ready\"}"))
+    }
+  }
+}

97e31102a51f4170a323003314a920960e2aaa44
YoshinoriN <nayo-msoft@outlook.jp>
2018-11-19 23:33:40 +0900

    Implement HttpServer object

---
+++ selfouettellia-server/src/main/scala/net/yoshinorin/selfouettellia/HttpServer.scala
@@ -0,0 +1,16 @@
+package net.yoshinorin.selfouettellia
+
+import scala.concurrent.ExecutionContext
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.stream.ActorMaterializer
+
+object HttpServer extends App with Route {
+
+  implicit val actorSystem: ActorSystem = ActorSystem("selfouettellia")
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+  implicit val executionContext: ExecutionContext = actorSystem.dispatcher
+
+  val bindingFuture = Http().bindAndHandle(route, config.HttpServer.host, config.HttpServer.port)
+
+}

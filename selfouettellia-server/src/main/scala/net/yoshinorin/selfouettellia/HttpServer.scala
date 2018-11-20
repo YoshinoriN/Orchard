70751264752bf05e4cfcdc77bac5b54285f8e541
YoshinoriN <nayo-msoft@outlook.jp>
2018-11-20 23:46:05 +0900

    Implement unbind at HttpServer

--- selfouettellia-server/src/main/scala/net/yoshinorin/selfouettellia/HttpServer.scala
+++ selfouettellia-server/src/main/scala/net/yoshinorin/selfouettellia/HttpServer.scala
@@ -1,6 +1,8 @@
 package net.yoshinorin.selfouettellia
 
 import scala.concurrent.ExecutionContext
+import scala.io.StdIn
+
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
@@ -12,5 +14,10 @@ object HttpServer extends App with Route {
   implicit val executionContext: ExecutionContext = actorSystem.dispatcher
 
   val bindingFuture = Http().bindAndHandle(route, config.HttpServer.host, config.HttpServer.port)
+
+  StdIn.readLine()
+  bindingFuture
+    .flatMap(_.unbind())
+    .onComplete(_ => actorSystem.terminate())
 
 }

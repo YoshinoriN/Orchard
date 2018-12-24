33b1b84761a28c520e1053781deaaebd7c482636
YoshinoriN <nayo-msoft@outlook.jp>
2018-12-24 23:50:55 +0900

    change order of CreateEvent case class fields

--- selfouettie-server/src/main/scala/net/yoshinorin/selfouettie/models/CreateEvent.scala
+++ selfouettie-server/src/main/scala/net/yoshinorin/selfouettie/models/CreateEvent.scala
@@ -3,7 +3,7 @@ package net.yoshinorin.selfouettie.models
 case class CreateEvent(
   eventId: Long,
   userName: String,
-  refType: String,
   ref: String,
+  refType: String,
   createdAt: Long
 ) extends BaseEvent[CreateEvent]

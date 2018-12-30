8de3c15f369e69733814d8bf2b903980f4f9c88a
YoshinoriN <nayo-msoft@outlook.jp>
2018-12-30 14:51:50 +0900

    rename IssueEvent to IssuesEvent

--- selfouettie-server/src/main/scala/net/yoshinorin/selfouettie/models/IssuesEvent.scala
+++ selfouettie-server/src/main/scala/net/yoshinorin/selfouettie/models/IssuesEvent.scala
@@ -1,10 +1,10 @@
 package net.yoshinorin.selfouettie.models
 
-case class IssueEvent(
+case class IssuesEvent(
   eventId: Long,
   userName: String,
   repositoryId: Long,
   issueNumber: Long,
   action: String,
   createdAt: Long
-) extends BaseEvent[IssueEvent]
+) extends BaseEvent[IssuesEvent]

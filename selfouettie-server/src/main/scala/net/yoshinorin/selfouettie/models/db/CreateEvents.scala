4d3403d3b28660ce2ecb65ba6e6c0d88b08ad930
YoshinoriN <nayo-msoft@outlook.jp>
2019-01-01 00:47:41 +0900

    fix CreateEvents field order

--- selfouettie-server/src/main/scala/net/yoshinorin/selfouettie/models/db/CreateEvents.scala
+++ selfouettie-server/src/main/scala/net/yoshinorin/selfouettie/models/db/CreateEvents.scala
@@ -5,7 +5,7 @@ import net.yoshinorin.selfouettie.models.BaseEvent
 case class CreateEvents(
   eventId: Long,
   userName: String,
-  ref: String,
   refType: String,
+  ref: String,
   createdAt: Long
 ) extends BaseEvent[CreateEvents]

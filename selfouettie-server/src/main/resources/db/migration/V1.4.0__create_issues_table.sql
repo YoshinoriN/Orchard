9442b1598990fbaaaf5a196bca0d7ed65db955a1
YoshinoriN <nayo-msoft@outlook.jp>
2019-01-02 20:32:22 +0900

    fix some ddl

--- selfouettie-server/src/main/resources/db/migration/V1.4.0__create_issues_table.sql
+++ selfouettie-server/src/main/resources/db/migration/V1.4.0__create_issues_table.sql
@@ -1,7 +1,6 @@
 CREATE TABLE issues (
-  user_name VARCHAR(255) NOT NULL,
   repository_id BIGINT UNSIGNED NOT NULL,
-  issue_number BIGINT UNSIGNED NOT NULL,
+  issue_number INT UNSIGNED NOT NULL,
   title TEXT NOT NULL,
   UNIQUE (repository_id, issue_number),
   FOREIGN KEY(repository_id) REFERENCES repositories(id)
--- selfouettie-server/src/main/resources/db/migration/V2.3.0__create_issue_events_table.sql
+++ selfouettie-server/src/main/resources/db/migration/V2.3.0__create_issue_events_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE issue_events (
   event_id BIGINT UNSIGNED PRIMARY KEY,
   user_name VARCHAR(255) NOT NULL,
   repository_id BIGINT UNSIGNED NOT NULL,
-  issue_number BIGINT UNSIGNED NOT NULL,
+  issue_number INT UNSIGNED NOT NULL,
   action ENUM ('opened','edited','deleted','transferred','closed','reopened','assigned','unassigned','labeled','unlabeled','milestoned','demilestoned'),
   created_at BIGINT UNSIGNED,
   FOREIGN KEY(event_id) REFERENCES events(id),

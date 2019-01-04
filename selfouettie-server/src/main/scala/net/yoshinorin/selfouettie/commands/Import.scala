ecb94eb64cdba5488ddadcf6b912ec12cc876b13
YoshinoriN <nayo-msoft@outlook.jp>
2019-01-04 13:02:32 +0900

    update import

--- selfouettie-server/src/main/scala/net/yoshinorin/selfouettie/commands/Import.scala
+++ selfouettie-server/src/main/scala/net/yoshinorin/selfouettie/commands/Import.scala
@@ -1,13 +1,14 @@
 package net.yoshinorin.selfouettie.commands
 
 import net.yoshinorin.selfouettie.services.EventsConverter
+import net.yoshinorin.selfouettie.services.EventService
 import net.yoshinorin.selfouettie.utils.File
 import net.yoshinorin.selfouettie.utils.Logger
 
 /**
  * Import GitHub events from JSON files
  */
-object Import extends App with EventsConverter with Logger {
+object Import extends App with EventsConverter with Logger with EventService {
 
   logger.info("Start import from JSON files.")
 
@@ -17,7 +18,12 @@ object Import extends App with EventsConverter with Logger {
         case Some(jsonFiles) => {
           jsonFiles.foreach(jsonFile => {
             val data = convert(File.readAll(jsonFile.getAbsolutePath))
-            //TODO: insert
+            data match {
+              case Some(x) => {
+                x.foreach(y => create(y))
+              }
+              case None => logger.info("Import records are nothing.")
+            }
           })
         }
         case None => logger.info("JSON file not found.")

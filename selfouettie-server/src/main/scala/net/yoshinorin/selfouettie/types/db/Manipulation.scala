881baa3dfab32df4ef4596d9469bdaf6003f0182
YoshinoriN <nayo-msoft@outlook.jp>
2019-01-15 21:55:54 +0900

    impl(type): update Manipulation class

--- selfouettie-server/src/main/scala/net/yoshinorin/selfouettie/types/db/Manipulation.scala
+++ selfouettie-server/src/main/scala/net/yoshinorin/selfouettie/types/db/Manipulation.scala
@@ -1,14 +1,15 @@
 package net.yoshinorin.selfouettie.types.db
 
-class SqlLimit(val limit: Int = 50) {}
+class Limit(val limit: Int = 50) {}
 
-object SqlLimit {
-  def apply(limit: Int = 50): SqlLimit = {
-    println(limit)
+object Limit {
+  def apply(limit: Int = 50): Limit = {
     if (limit < 1 || limit > 50) {
-      new SqlLimit(50)
+      new Limit(50)
     } else {
-      new SqlLimit(limit)
+      new Limit(limit)
     }
   }
 }
+
+case class EpochSecondFromTo(from: Int, to: Int)
--- selfouettie-server/src/test/scala/net/yoshinorin/selfouettie/types/db/ManipulationSpec.scala
+++ selfouettie-server/src/test/scala/net/yoshinorin/selfouettie/types/db/ManipulationSpec.scala
@@ -5,28 +5,28 @@ import org.scalatest.FunSuite
 class ManipulationSpec extends FunSuite {
 
   test("Less than default value") {
-    val l: SqlLimit = SqlLimit(49)
+    val l: Limit = Limit(49)
     assert(l.limit == 49)
   }
 
   test("Omit argument") {
-    val l: SqlLimit = SqlLimit()
+    val l: Limit = Limit()
     assert(l.limit == 50)
   }
 
   test("Argument is one") {
-    val l: SqlLimit = SqlLimit(1)
+    val l: Limit = Limit(1)
 
     assert(l.limit == 1)
   }
 
   test("Less than one") {
-    val l: SqlLimit = SqlLimit(0)
+    val l: Limit = Limit(0)
     assert(l.limit == 50)
   }
 
   test("Exceed default value") {
-    val l: SqlLimit = SqlLimit(51)
+    val l: Limit = Limit(51)
     assert(l.limit == 50)
   }
 

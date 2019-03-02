8e0f26ed59201159b660a510077028e86fde48eb
YoshinoriN <nayo-msoft@outlook.jp>
2019-03-02 21:35:26 +0900

    feat: run GitHubEventActorService only production mode

--- orchard-server/src/main/scala/net/yoshinorin/orchard/BootStrap.scala
+++ orchard-server/src/main/scala/net/yoshinorin/orchard/BootStrap.scala
@@ -5,10 +5,14 @@ import net.yoshinorin.orchard.commands.db.Migrate
 import net.yoshinorin.orchard.http.HttpServer
 import net.yoshinorin.orchard.utils.Logger
 
-object BootStrap extends App with Logger with GitHubEventActorService {
+object BootStrap extends App with Logger {
 
   Migrate.run
+
+  if (!args.contains("dev") && !args.contains("-d")) {
+    GitHubEventActorService.initialize()
+  }
+
   HttpServer.run(args.toList)
-  initializeGitHubEventActor()
 
 }

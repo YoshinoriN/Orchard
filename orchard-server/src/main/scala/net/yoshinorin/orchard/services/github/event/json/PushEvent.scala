c371cecee6643387311ce9dc6d1a2faa830c164f
YoshinoriN <nayo-msoft@outlook.jp>
2019-03-16 22:19:58 +0900

    feat: implement net.yoshinorin.orchard.services.github.event.json.PushEvent

---
+++ orchard-server/src/main/scala/net/yoshinorin/orchard/services/github/event/json/PushEvent.scala
@@ -0,0 +1,40 @@
+package net.yoshinorin.orchard.services.github.event.json
+
+import io.circe.{Decoder, Json}
+import net.yoshinorin.orchard.models.db.{Events, PushEvents}
+import net.yoshinorin.orchard.utils.Logger
+
+class PushEvent(event: Events, json: Json) extends JsonBase[PushEvents] with Logger {
+
+  val issuesEvent: Option[PushEvents] = this.convert
+
+  /**
+   * Get Converted case class
+   *
+   * @return
+   */
+  override def getConvertedCaseClass: Option[PushEvents] = this.issuesEvent
+
+  /**
+   * Convert JSON to PushEvents case class
+   *
+   * @return
+   */
+  override def convert: Option[PushEvents] = {
+    val ref: Decoder.Result[String] = json.hcursor.downField("payload").get[String]("ref")
+    val size: Decoder.Result[Int] = json.hcursor.downField("payload").get[Int]("size")
+
+    if (ref.isRight && size.isRight) {
+      Some(PushEvents(event.id, event.userName, event.repositoryId, ref.right.get, size.right.get, event.createdAt))
+    } else {
+      logger.error(s"Event id [${event.id}]. Failed parse to CreateEvents.")
+      None
+    }
+  }
+}
+
+object PushEvent {
+
+  def apply(event: Events, json: Json): PushEvent = new PushEvent(event, json)
+
+}

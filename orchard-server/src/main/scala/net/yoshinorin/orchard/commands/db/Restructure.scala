7cc98f87ba35c42ca1c49f79f243ea9d8b338d95
YoshinoriN <nayo-msoft@outlook.jp>
2019-03-28 00:27:13 +0900

    chore(log): fix log level for Restructure

--- orchard-server/src/main/scala/net/yoshinorin/orchard/commands/db/Restructure.scala
+++ orchard-server/src/main/scala/net/yoshinorin/orchard/commands/db/Restructure.scala
@@ -17,27 +17,27 @@ object Restructure extends App with QuillProvider with ConfigProvider with Logge
 
   val schema: String = configuration.getString("db.restructure.schema")
 
-  logger.info("DROP SCHEMA")
+  logger.debug("DROP SCHEMA")
   probe(s"DROP SCHEMA $schema")
-  logger.info("DROP SCHEMA COMPLETE!!")
+  logger.debug("DROP SCHEMA COMPLETE!!")
 
-  logger.info("CREATE SCHEMA")
+  logger.debug("CREATE SCHEMA")
   probe(s"CREATE DATABASE $schema")
-  logger.info("CREATE SCHEMA COMPLETE!!")
+  logger.debug("CREATE SCHEMA COMPLETE!!")
 
-  logger.info("STARTING DB MIGRATION")
+  logger.debug("STARTING DB MIGRATION")
   val flyway = Flyway
     .configure()
     .dataSource(DataBaseConfig.url, DataBaseConfig.user, DataBaseConfig.password)
     .load()
 
   flyway.migrate
-  logger.info("FINISH DB MIGRATION")
+  logger.debug("FINISH DB MIGRATION")
 
   if (configuration.getBoolean("db.restructure.importData")) {
-    logger.info("IMPORT DATA FROM JSON")
+    logger.debug("IMPORT DATA FROM JSON")
     Import.main(Array())
-    logger.info("FINISH IMPORT DATA FROM JSON")
+    logger.debug("FINISH IMPORT DATA FROM JSON")
   }
 
 }
